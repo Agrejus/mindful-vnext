2b3bbcb9642335e8f6d257cc84678f725d85bd5b
James DeMeuse <james.demeuse@gmail.com>
2023-02-22 11:54:55 -0600

    fixed popover

--- src/app/shared-components/toolbar/content-toolbar-buttons/ContentToolbarTallDropdownButton.tsx
+++ src/app/shared-components/toolbar/content-toolbar-buttons/ContentToolbarTallDropdownButton.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button, Popover, Typography } from '@mui/material';
+import React, { useState } from 'react';
 
 export interface IContentToolbarTallDropdownButtonProps {
     icon: React.ReactNode;
@@ -10,12 +10,33 @@ export interface IContentToolbarTallDropdownButtonProps {
 export const ContentToolbarTallDropdownButton: React.FC<IContentToolbarTallDropdownButtonProps> = (props) => {
 
     const { icon, label, onClick } = props;
+    const [anchorElement, setAnchorElement] = useState<HTMLButtonElement | null>(null);
 
-    return <Button variant="contained" className="mui-tall-button mui-tall-button-dropdown" onClick={onClick}>
-        <div className='mui-tall-button-icon-container'>
-            {icon}
-        </div>
-        <span className='mui-tall-button-label'>{label.split(' ').map((w, i) => <>{w}{i % 2 === 0 ? <br/> : null}</>)}</span>
-        <i className="bi bi-caret-down-fill"></i>
-    </Button>
+    const open = Boolean(anchorElement);
+    const id = open ? 'mui-tall-button-popover' : undefined;
+
+    return <>
+        <Button variant="contained" className="mui-tall-button mui-tall-button-dropdown" onClick={e => setAnchorElement(e.currentTarget)}>
+            <div className='mui-tall-button-icon-container'>
+                {icon}
+            </div>
+            <span className='mui-tall-button-label'>{label.split(' ').map((w, i) => <>{w}{i % 2 === 0 ? <br /> : null}</>)}</span>
+            <i className="bi bi-caret-down-fill"></i>
+        </Button>
+        <Popover
+            className='mui-tall-button-dropdown-pop-over'
+            id={id}
+            open={open}
+            anchorEl={anchorElement}
+            onClose={() => setAnchorElement(null)}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+            }}
+        >
+            <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+            <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+            <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+        </Popover>
+    </>
 }
